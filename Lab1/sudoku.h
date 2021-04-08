1c222a937573da1d3e76edab55e442c6922a6c8c
WGY <1070788157@qq.com>
2021-04-09 00:07:58 +0800

    docs: 提交性能分析报告

--- Lab1/sudoku.h
+++
@@ -1,7 +0,0 @@
-#ifndef SUDOKU_H
-#define SUDOKU_H
-
-enum { ROW=9, COL=9, N = 81, NEIGHBOR = 20 };
-bool solve_sudoku_dancing_links(int *board);
-
-#endif
--- Lab1/tools.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TOOLS_H__
-#define __TOOLS_H__
-
-int getCpuInfo();
-//File getFile(const char *);
-void debug_tool(const char a[]);
-#endif
