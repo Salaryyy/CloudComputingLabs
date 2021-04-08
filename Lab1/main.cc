fb24ebc1ad6c7d4a05bdf6bdfdce5090e1e01c0a
lizengyi <1095609182@qq.com>
2021-04-08 21:03:24 +0800

    style: 增添注释

--- Lab1/main.cc
+++ Lab1/main.cc
@@ -13,42 +13,53 @@
 #include <list>
 #include "sudoku.h"
 #include "tools.h"
-int n_pthread; // 在这里修改线程数量
 
-pthread_mutex_t visit_buf = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock_print = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock_file = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
-pthread_cond_t full = PTHREAD_COND_INITIALIZER;
-pthread_cond_t tj = PTHREAD_COND_INITIALIZER;
-pthread_cond_t *print_order;
-pthread_t *tid;
-pthread_t file_thread;
-int total = 0;
-int cur_print = 0;
+pthread_mutex_t visit_buf = PTHREAD_MUTEX_INITIALIZER;    //缓冲区锁
+pthread_mutex_t lock_print = PTHREAD_MUTEX_INITIALIZER;   //打印锁
+pthread_mutex_t lock_file = PTHREAD_MUTEX_INITIALIZER;    //文件队列锁
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;          //生产者挂起
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;           //消费者挂起
+//pthread_cond_t tj = PTHREAD_COND_INITIALIZER;           //文件队列为空时消费者挂起
+pthread_cond_t *print_order;                              //控制输出顺序
+pthread_t *tid;                                           //解题线程
+pthread_t file_thread;                                    //读文件线程
 
-int finish_num = 0;
-char **buf;
-int fill_ptr = 0;
-int use_ptr = 0;
-int n_data = 0;
-bool end = false;
+int n_pthread;          //线程数量
+int total = 0;          //解决问题总数
+int cur_print = 0;      //当前打印者编号
+int finish_num = 0;     //当前打开的文件 线程完成的数量
+char **buf;             //缓冲区，存放题目
+int fill_ptr = 0;       //生产下标
+int use_ptr = 0;        //消费下标
+int n_data = 0;         //缓冲区剩余题目个数
+//bool endfile = false; //是否读到文件尾
 
-std::list<char *> file_list;
+std::list<char *> file_list; //文件名队列 
 
 void init()
 {
-    int n = getCpuInfo();
-    print_order = (pthread_cond_t *)malloc(n * sizeof(pthread_cond_t));
-    buf = (char **)malloc(n * sizeof(char *));
-    tid = (pthread_t *)malloc(n * sizeof(pthread_t));
+    //获取cpu核心数
+    int n = getCpuInfo(); 
     n_pthread = n;
+    //缓冲区开辟n行
+    buf = (char **)malloc(n * sizeof(char *));
+    for (int i = 0; i < n_pthread; ++i)
+        buf[i] = (char *)malloc(83);
+    //n个条件变量控制输出
+    print_order = (pthread_cond_t *)malloc(n * sizeof(pthread_cond_t));
+    //n个线程号
+    tid = (pthread_t *)malloc(n * sizeof(pthread_t));
+    //初始化 输出顺序的条件变量
+    for (int i = 0; i < n_pthread; ++i){
+        print_order[i] = PTHREAD_COND_INITIALIZER;
+    }
 }
 
+//释放malloc的空间
 void program_end()
 {
     free(print_order);
-    //释放缓冲区
+    free(tid);
     for (int i = 0; i < n_pthread; ++i)
         free(buf[i]);
     free(buf);
@@ -70,64 +81,49 @@ void *solver(void *arg)
     while (1)
     {
         pthread_mutex_lock(&visit_buf);
-        // if (n_data == 0 && end)
-        // { //文件读到尾了
-        //     finish_num++;
-        //     pthread_mutex_unlock(&visit_buf); //开锁跑路
-        //     //printf("paolu\n");
-        //     pthread_cond_wait(&tj, &visit_buf);
-        //     //pthread_exit(NULL);
-        // }
-        // while (n_data == 0 && !end) //这里类似于线程池的解法
-        // {
-        //     pthread_cond_wait(&full, &visit_buf);
-        // }
-        while (n_data == 0) //这里类似于线程池的解法
-        {
+        while (n_data == 0){
             pthread_cond_wait(&full, &visit_buf);
         }
-        ++total;
-        int record_print = total; //调试用
-        int myturn = (total - 1) % n_pthread;
-        //printf("消费%d\n", cur_print);
+
+        ++total;                              //当前数据的行数
+        //int record_print = total;           //调试用
+        int myturn = (total - 1) % n_pthread; //应该的打印顺序
+
+        //读一行题放到board里面
         char *data = get();
-        for (int i = 0; i < 81; ++i)
-        {
+        for (int i = 0; i < 81; ++i){
             board[i] = data[i] - '0';
         }
+
         pthread_cond_signal(&empty);
         pthread_mutex_unlock(&visit_buf);
 
         //解决问题
         solve_sudoku_dancing_links(board);
 
-        //这里还有个大问题，输出顺序
+        //输出顺序
         pthread_mutex_lock(&lock_print);
-        while (myturn != cur_print)
-        {
-            //printf("第%d行想打印，但是没轮到我, 在%d号条件变量上等待\n", record_print, myturn);
+        while (myturn != cur_print){    //没有轮到则在自己的条件变量上等待
             pthread_cond_wait(&print_order[myturn], &lock_print);
         }
 
         //打印到屏幕 注释掉可以省不少时间
-        //printf("现在打印第%d行", record_print);
         for (int i = 0; i < 81; ++i)
             printf("%d", board[i]);
-        //printf("%d", board[i]);
         printf("\n");
-        //printf("唤醒%d\n", (myturn + 1) % n_pthread);
 
-        cur_print = (cur_print + 1) % n_pthread;
-        pthread_cond_signal(&print_order[(myturn + 1) % n_pthread]);
+        cur_print = (cur_print + 1) % n_pthread;                     //下一个该打印的编号
+        pthread_cond_signal(&print_order[(myturn + 1) % n_pthread]); //唤醒下一个，如果对方在睡的话
+        //这里也可以只用一个条件变量，到这里用broadcast唤醒所有其他线程，但是效率可能会低一点，没有尝试-.-
         pthread_mutex_unlock(&lock_print);
     }
 }
 
-void *file_handler(void *argv)
+//处理从屏幕读入的文件名，加入文件队列
+void *file_handler(void *argv) 
 {
     char tmp[20];
-    while (1)
-    {
+    while(1){
         scanf("%s", tmp);
         pthread_mutex_lock(&lock_file);
         file_list.push_back(tmp);
@@ -141,78 +137,59 @@ int main(int argc, char *argv[])
 
     init();
 
-    //开辟缓冲区
-    for (int i = 0; i < n_pthread; ++i)
-        buf[i] = (char *)malloc(83);
-    //初始化 输出顺序 条件变量
-    for (int i = 0; i < n_pthread; ++i)
-    {
-        print_order[i] = PTHREAD_COND_INITIALIZER;
-        pthread_create(&tid[i], NULL, solver, NULL);
+    for (int i = 0; i < n_pthread; ++i){
+        pthread_create(&tid[i], NULL, solver, NULL);        //解题
     }
-    pthread_create(&file_thread, NULL, file_handler, NULL);
-    while (1)
-    {
-        while (file_list.size() == 0)
-            ;
+
+    pthread_create(&file_thread, NULL, file_handler, NULL); //读文件名
+    while(1){
+
+        while(file_list.size() == 0);  //这里可以加个cond，算了没啥必要-.-
+        //printf("%d", file_list.size());
+        //从队列里面获取一个文件名, 反正只有一个生产者, 下来就肯定有文件可读，所以就不加cond了
         pthread_mutex_lock(&lock_file);
-        {
-            strcpy(filename, file_list.front());
-            file_list.pop_front();
-        }
+        strcpy(filename, file_list.front());
+        file_list.pop_front();
         pthread_mutex_unlock(&lock_file);
-        if (strcmp(filename, "quit") == 0)
-        {
+        
+        if(strcmp(filename, "quit") == 0){
             printf("退出\n");
             exit(0);
         }
-        if (access(filename, F_OK) == -1)
-        {
+
+        if(access(filename, F_OK) == -1){
             printf("文件不存在\n");
             continue;
         }
-        //finish_num = 0;
-        //scanf("%s", filename);
+
         FILE *fp = fopen(filename, "r");
-        total = 0;
-        //int64_t start = now();
-        //pthread_cond_broadcast(&tj);
-        while (1)
-        { //生产者
+        total = 0;           //重新开始计数
+        cur_print = 0;       //重新从第0行开始打印
+
+        while (1){ //生产者
             pthread_mutex_lock(&visit_buf);
-            while (n_data == n_pthread)
-            {
-                //printf("缓冲区满了，生产者释放锁\n");
+
+            while (n_data == n_pthread){    //缓冲区满了
                 pthread_cond_wait(&empty, &visit_buf);
             }
-            //生产开始
-            if (fgets(buf[fill_ptr], 83, fp) != NULL)
-            { //put函数在这里
-                //printf("生产%d\n", total);
+
+            //put函数在这里, 从文件读一行放到缓冲区
+            if(fgets(buf[fill_ptr], 83, fp) != NULL){
                 fill_ptr = (fill_ptr + 1) % n_pthread;
                 ++n_data;
             }
-            else
-            {
-                //end = true; //读到文件末尾了
+            //读到文件末尾了，算一次无效读入，去取下一个文件名
+            else{
                 pthread_mutex_unlock(&visit_buf);
-                //pthread_cond_broadcast(&full); //广播唤醒所有消费者 没有数据了
                 break;
             }
             pthread_cond_signal(&full);
             pthread_mutex_unlock(&visit_buf);
         }
-        // while (finish_num != n_pthread)
-        //     ;
-
-        // int64_t end = now();
-        // double sec = (end - start) / 1000000.0;
-        // printf("%f sec %f ms each %d, \n", sec, 1000 * sec / total, total);
     }
 
-    //等待子线程全部完成
-    for (int i = 0; i < n_pthread; ++i)
-    {
+    //等待子线程全部完成 这里其实执行不到
+    for (int i = 0; i < n_pthread; ++i){
         pthread_join(tid[i], NULL);
     }
 
