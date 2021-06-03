7975d6268b5e7bdfeb0548216ef767a84e8b06a7
lizengyi <1095609182@qq.com>
2021-06-03 17:56:31 +0800

    feat: 读取配置文件

---
+++ Lab3/src/conf/conf.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <fstream>
+
+#include <unistd.h>
+#include <string.h>
+
+
+struct Part
+{
+    std::string ip;
+    int port;
+};
+
+struct Conf
+{
+    bool isCoor;
+    std::string coorIp;
+    int coorPort;
+    int partNum;
+    std::vector<Part> part;
+};
+
+Conf getConf(std::string filename);
+bool checkConf(const std::string& name);
