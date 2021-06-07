6ea1078da2ee2024db10847981d55861e413a8dc
Salaryyy <2639511626@qq.com>
2021-06-07 21:31:29 +0800

    feat: 最近修改

--- Lab3/src/coordinator.cpp
+++ Lab3/src/coordinator.cpp
@@ -12,10 +12,12 @@
 #include <pthread.h>
 #include <sys/socket.h>
 #include <signal.h>
+#include <sys/time.h>
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <list>
 #include <cstring>
 #include "tools.h"
 #include "conf.h"
@@ -23,34 +25,50 @@ using namespace std;
 
 
 // 包头结构 data包应该还要细分 有的是控制信息包 有的是数据包
-enum Type {heart, data};
-struct packet_head
-{
+enum Type {heart, data, done, commit, rollback};
+struct packet_head{
     Type type;
     int length;
 };
 
+struct client_ask{
+    int fd; // 处理完后记得关闭连接
+    string message;//或者拆得更细
+};
+
 void *check_participant(void *argv);
+void *receive_client(void *argv);
+void *hand_out(void *argv);
 
 class coordinator{
 private:
+    // 配置信息
     unsigned int myip;
     unsigned short myport;
     int n_participant;
-    int listenfd;
     vector<unsigned int> ip_participant;
     vector<unsigned short> port_participant;
     vector<int> fd_participant;
     Conf myconf;
+    int listenfd;
     struct sockaddr_in servaddr;
 
-    // 心跳变量
-    pthread_mutex_t write_lock;
+    // 参与者
+    vector<pthread_mutex_t> write_lock;
     pthread_mutex_t heart_lock;
+    //pthread_mutex_t data_lock;
     // 记录没有收到的心跳个数
     vector<int>  count_heart;
     vector<bool> recv_heart;
     vector<bool> shut_down;
+    vector<string> data_return;
+    vector<bool> recv_data;
+    //int alive;
+    
+    // 客户端
+    list<client_ask> worklist;
+    pthread_mutex_t worklist_lock;
+    pthread_cond_t worklist_cond;
     
 
 public:
@@ -72,16 +90,27 @@ public:
             fd_participant[i] = -1;
         }
         
-        write_lock = PTHREAD_MUTEX_INITIALIZER;
+        //write_lock = PTHREAD_MUTEX_INITIALIZER;
         heart_lock = PTHREAD_MUTEX_INITIALIZER;
+        //data_lock = PTHREAD_MUTEX_INITIALIZER;
+        write_lock.resize(n_participant);
         count_heart.resize(n_participant);
         recv_heart.resize(n_participant);
         shut_down.resize(n_participant);
+        data_return.resize(n_participant);
+        recv_data.resize(n_participant);
+
         for(int i = 0; i < n_participant; ++i){
             count_heart[i] = 0;
             recv_heart[i] = false;
             shut_down[i] = false;
+            recv_data[i] = false;
+            write_lock[i] = PTHREAD_MUTEX_INITIALIZER;
         }
+        //alive = n_participant;
+
+        worklist_lock = PTHREAD_MUTEX_INITIALIZER;
+        worklist_cond = PTHREAD_COND_INITIALIZER;
     }
 
     void init_serveraddr(){
@@ -119,7 +148,7 @@ public:
             int connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &cliaddr_len);   
             
             //看一下客户端信息
-            char client_ip[1024];
+            char client_ip[40];
             int  client_port = ntohs(cliaddr.sin_port);
             inet_ntop(AF_INET, &cliaddr.sin_addr, client_ip, sizeof(client_ip));
             printf("received from %s at PORT %d\n", client_ip, client_port);
@@ -189,44 +218,146 @@ public:
             // 心跳包
             if(head.type == heart){
 
-                // 这里得加点锁
+                
                 pthread_mutex_lock(&heart_lock);
                 count_heart[i] = 0;        // 每次收到心跳包，count置0
                 recv_heart[i] = true;
                 pthread_mutex_unlock(&heart_lock);
                 cout<<"收到来自参与者"<<i<<"的心跳包"<<endl;
+
+                // 这里得加锁
                 // 发回去
+                pthread_mutex_lock(&write_lock[i]);
                 send(fd_participant[i], &head, sizeof(head), 0);
+                pthread_mutex_unlock(&write_lock[i]);
                 cout<<"给参与者"<<i<<"回应心跳包"<<endl;
             }
             
-            // 数据包
-            else{
-                // 数据包，通过head.length确认数据包长度 
+            // 数据包 get的数据  del的个数 都是原封不动地返回 但是del需要完成两阶段后返回
+            else if(head.type == data){
+                // 数据包，通过head.length确认数据包长度  好像这个参数没必要用了
+                //pthread_mutex_lock(&data_lock);
+                // 告诉handout线程 我正在读你等会
+                ret = readn(fd_participant[i], data_return[i]);
+                if(ret > 0){
+                    recv_data[i] = true;
+                }
+                //pthread_mutex_unlock(&data_lock);
             }   
 
 
         }
     }
 
-    void receive_client(){
-        ;
-    }
+    
 
     // 定期检查参与者是否活着
     friend void *check_participant(void *argv);
 
+    // 负责监听普通客户端的请求
+    friend void *receive_client(void *argv);
+
+    // 分发任务
+    friend void *hand_out(void *argv);
+
     void run(){
-        pthread_t tid;
-        pthread_create(&tid, NULL, check_participant, (void *)this);
+        pthread_t tid1, tid2, tid3;
+        pthread_create(&tid1, NULL, check_participant, (void *)this);
+        pthread_create(&tid2, NULL, receive_client, (void *)this);
+        pthread_create(&tid3, NULL, hand_out, (void *)this);
         while(1){
             receive_participant();
-            //receive_client();
         }
     }
 
     
 };
+
+void *hand_out(void *argv){
+    coordinator *p = (coordinator *)argv;
+    while(1){
+        // 从任务队列里面获取一个任务
+        pthread_mutex_lock(&(p->worklist_lock));
+        while(p->worklist.empty()){ //if也行 没有相同的线程在竞争
+            pthread_cond_wait(&(p->worklist_cond), &(p->worklist_lock));
+        }
+
+        client_ask ask = p->worklist.front();
+        p->worklist.pop_front();
+        pthread_mutex_unlock(&(p->worklist_lock));
+
+        // 判断一下是什么请求
+        /*
+            if(ask[i] == 'G')
+                return 1;
+            if(ask[i] == 'S')
+                return 2;
+            if(ask[i] == 'D')
+                return 3;
+        */
+        /*
+            get
+            找一个能在运行的参与者直接发过去 然后把响应直接给客户端    
+            如果没有响应 则换下个人人发    (定时) 要不要换呢 麻烦...
+        */
+        if(get_type(ask.message) == 1){
+            int i = 0;
+            // 干脆访问shutdown不加锁了 竞争比较小 
+            // pthread_mutex_lock(&(p->heart_lock));
+            again:
+            while(i < p->n_participant && p->shut_down[i])
+                ++i;
+            // pthread_mutex_unlock(&(p->heart_lock));
+            if(i == p->n_participant){
+                ;
+                //return error message continue
+            }
+            // 理论上每个协调者都需要一个锁  
+            /*
+                锁住
+                发送请求
+                开锁
+                等会 怎么个等法 等多久 没有shutdown就一直等   检查recvdatai
+                接受回复转发
+            */
+            pthread_mutex_lock(&(p->write_lock[i]));
+            writen(p->fd_participant[i], ask.message);
+            pthread_mutex_unlock(&(p->write_lock[i]));
+            // 啊临界区统统不加锁了...
+            while(!p->shut_down[i]){
+                if(p->recv_data[i])
+                    break;
+            }
+            if(p->shut_down[i])
+                goto again;
+            //pthread_mutex_lock(&(p->data_lock));  //这个锁是不是不用啊  每次都是读完了再叫这个线程过来取得 读完后recvdata才会更新
+            ask.message = p->data_return[i];
+            p->data_return[i].clear();
+            p->recv_data[i] = false; //重置缓冲区状态
+            //pthread_mutex_unlock(&(p->data_lock)); 
+            writen(ask.fd, ask.message);
+            close(ask.fd);
+        }
+
+        /*
+            heart 锁的临界区太长了 可能无法正常收发心跳包
+            del or set
+            两阶段
+            1 给每一个正在运行的参与者发送请求
+            2 参与者收到请求(然后把请求做了) 给协调者确认
+            3 协调者收到确认
+              所有人都确认，给每个人确认包commit  有人没确认(定时) 给每个人发送回滚包rollback
+            4 参与者收到确认包commit， 收到回滚包或者超时，rollback   有可能有人没收到这个 还没发完所有commit协调者断掉了怎么办
+
+            1 给每一个正在运行的参与者发送请求
+            2 参与者收到请求 记录这个串 回个ready
+            3 协调者如果收到所有ready 则commit 否则 rollback
+
+        */
+
+
+    }
+}
 
 void *check_participant(void *argv){
     coordinator *p = (coordinator *)argv;
@@ -236,6 +367,7 @@ void *check_participant(void *argv){
         pthread_mutex_lock(&(p->heart_lock));
 
         for(int i = 0; i < p->n_participant; ++i){
+            // 这里如果断了 就不用检测了
             if(p->recv_heart[i]){
                 p->recv_heart[i] = false;
             }
@@ -249,11 +381,63 @@ void *check_participant(void *argv){
 
                     // 要求2
                     p->shut_down[i] = true;
+                    //--(p->alive);
+
+                    // 要求3
+                    // 开个子线程去调用reconnecte_participant();
                 }
             }
         }
 
         pthread_mutex_unlock(&(p->heart_lock));
+    }
+}
+
+void *receive_client(void *argv){
+    coordinator *p = (coordinator *)argv;
+
+    struct sockaddr_in cliaddr;
+    socklen_t cliaddr_len = sizeof(cliaddr);
+    while(1){
+        // 如果系统正在恢复 或者当前参与者全部失效了 阻塞在这里
+        // ...
+
+        //阻塞监听链接请求 这个连接可能来自参与者 可能来自客户端 需要判断一下
+        cout<<"等待客户端连接中"<<endl;
+        int connfd = accept(p->listenfd, (struct sockaddr *)&cliaddr, &cliaddr_len);   
+
+        //看一下客户端信息
+        char client_ip[40];
+        int  client_port = ntohs(cliaddr.sin_port);
+        inet_ntop(AF_INET, &cliaddr.sin_addr, client_ip, sizeof(client_ip));
+        printf("新客户端连接ip: %s, port: %d\n", client_ip, client_port);
+        printf("新连接cfd: %d\n", connfd);
+
+        // 检查是否是参与者
+        bool flag = false;
+        for(int i = 0; i < p->n_participant; ++i){
+            if(ip_int(client_ip) == p->ip_participant[i] && client_port == p->port_participant[i]){
+                cout<<"这不是普通客户端"<<endl;
+                close(connfd);
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            continue;
+        }
+        // 只在这边解析方法 具体工作在hand_out里面做
+        string data;
+        int ret = readn(connfd, data);
+        if(ret > 0){
+            //cout<<data<<endl;
+            pthread_mutex_lock(&(p->worklist_lock));
+            // emm能不能用右值引用呢 拷贝好几次有很大额外开销
+            p->worklist.push_back({connfd, data});
+            pthread_cond_signal(&(p->worklist_cond)); // 唤醒正在等待的hand_out线程
+            pthread_mutex_unlock(&(p->worklist_lock));
+        }
+
     }
 }
 
--- Lab3/src/tools.cpp
+++ Lab3/src/tools.cpp
@@ -11,66 +11,13 @@
 #include <ctype.h>
 #include <sys/socket.h>
 #include <stdbool.h>
-#include <regex.h>
+#include <sstream>
 #include "tools.h"
+using namespace std;
 
 
-//static char errbuf[ERROR_SIZE] = {0};
 
-//合法ip的正则
-//static const char *ip_format = "(25[0-4]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[1-9])[.](25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])[.](25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])[.](25[0-4]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[1-9])";
-//static const char *ip_format = "^([0-9]|[1-9][0-9]|1[0-9]{1,2}|2[0-4][0-9]|25[0-4]).([0-9]|[1-9][0-9]|1[0-9]{1,2}|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9]{1,2}|2[0-4][0-9]|25[0-5]).([1-9]|[1-9][0-9]|1[0-9]{1,2}|2[0-4][0-9]|25[0-5])$";
 
-// bool get_IP_legal(regex_t *ipreg, const char *ip){
-// 	regmatch_t pmatch[1];
-// 	const size_t nmatch = 1;
-// 	int status = regexec(ipreg, ip, nmatch, pmatch, 0);
-// 	if(status == 0) {
-// 		//printf("Match Successful!\n");
-//         return true;
-// 	}
-// 	else if(status == REG_NOMATCH) {
-// 		regerror(status, ipreg, errbuf, ERROR_SIZE);	
-// 		//printf("%s\n", errbuf);
-//         memset(errbuf, 0, ERROR_SIZE);
-//         return false;
-		
-// 	}
-//     return true;
-// }
-
-bool get_name_id(char *str, int len, char *name, char *id){
-    int spite = 0;
-    while(spite < len && str[spite] != '&')
-        ++spite;
-    if(spite == len)
-        return false;
-
-    // 获取name
-    int equal1 = 0;
-    while(equal1 < spite && str[equal1] != '=')
-        ++equal1;
-    if(!(equal1 == 4 && str[0] == 'N' && str[1] == 'a' && str[2] == 'm' && str[3] == 'e'))
-        return false;
-    int index1 = 0;
-    for(int i = equal1 + 1; i < spite; ++i){
-        name[index1++] = str[i];
-    }
-    name[index1] = '\0';
-
-    // 获取id
-    int equal2 = spite + 1;
-    while(equal2 < len && str[equal2] != '=')
-        ++equal2;
-    if(!(equal2 - spite == 3 && str[spite + 1] == 'I' && str[spite + 2] == 'D'))
-        return false;
-    int index2 = 0;
-    for(int i = equal2 + 1; i < len; ++i){
-        id[index2++] = str[i];
-    }
-    id[index2] = '\0'; 
-    return true;
-}
 
 // ip转换成int 进行非法判断
 unsigned int ip_int(char *ip)
@@ -106,47 +53,7 @@ unsigned int ip_int(char *ip)
     return re;
 }
 
-// 通过文件名获取文件的类型
-// const char *get_file_type(const char *name)
-// {
-//     char* dot;
-//     // 自右向左查找‘.’字符, 如不存在返回NULL
-//     dot = strrchr(name, '.');   
-//     if (dot == NULL)
-//         return "text/plain; charset=utf-8";
-//     if (strcmp(dot, ".html") == 0 || strcmp(dot, ".htm") == 0)
-//         //return "text/html; charset=utf-8";
-//         return "text/html";
-//     if (strcmp(dot, ".jpg") == 0 || strcmp(dot, ".jpeg") == 0)
-//         return "image/jpeg";
-//     if (strcmp(dot, ".gif") == 0)
-//         return "image/gif";
-//     if (strcmp(dot, ".png") == 0)
-//         return "image/png";
-//     if (strcmp(dot, ".css") == 0)
-//         return "text/css";
-//     if (strcmp(dot, ".au") == 0)
-//         return "audio/basic";
-//     if (strcmp( dot, ".wav" ) == 0)
-//         return "audio/wav";
-//     if (strcmp(dot, ".avi") == 0)
-//         return "video/x-msvideo";
-//     if (strcmp(dot, ".mov") == 0 || strcmp(dot, ".qt") == 0)
-//         return "video/quicktime";
-//     if (strcmp(dot, ".mpeg") == 0 || strcmp(dot, ".mpe") == 0)
-//         return "video/mpeg";
-//     if (strcmp(dot, ".vrml") == 0 || strcmp(dot, ".wrl") == 0)
-//         return "model/vrml";
-//     if (strcmp(dot, ".midi") == 0 || strcmp(dot, ".mid") == 0)
-//         return "audio/midi";
-//     if (strcmp(dot, ".mp3") == 0)
-//         return "audio/mpeg";
-//     if (strcmp(dot, ".ogg") == 0)
-//         return "application/ogg";
-//     if (strcmp(dot, ".pac") == 0)
-//         return "application/x-ns-proxy-autoconfig";
-//     return "text/plain; charset=utf-8";
-// }
+
 
 // 读取http请求的一行
 int get_line(int sock, char *buf, int size)
@@ -226,3 +133,147 @@ std::string getOptConf(int argc, char **argv)
     }
     return name;
 }
+
+
+ssize_t readn(int fd, std::string &inBuffer){
+    ssize_t nread = 0;
+    ssize_t readSum = 0;
+    while(true){
+        char buff[MAX_BUFF];
+        if((nread = read(fd, buff, MAX_BUFF)) < 0){
+            if(errno == EINTR)
+                continue;
+            else if(errno == EAGAIN){
+                return readSum;
+            }  
+            else{
+                perror("read error");
+                return -1;
+            }
+        }
+        else if(nread == 0){
+            break;
+        }
+        readSum += nread;
+        inBuffer += std::string(buff, buff + nread);
+    }
+    return readSum;
+}
+
+ssize_t writen(int fd, std::string &sbuff){
+    size_t nleft = sbuff.size();
+    ssize_t nwritten = 0;
+    ssize_t writeSum = 0;
+    const char *ptr = sbuff.c_str();
+    while(nleft > 0){
+        if((nwritten = write(fd, ptr, nleft)) <= 0){
+            if(nwritten < 0){
+                if(errno == EINTR){
+                    nwritten = 0;
+                    continue;
+                }
+                else if(errno == EAGAIN)
+                    break;
+                else
+                    return -1;
+            }
+        }
+        writeSum += nwritten;
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    // 传完的清除掉
+    if(writeSum == static_cast<int>(sbuff.size()))
+        sbuff.clear();
+    else
+        sbuff = sbuff.substr(writeSum);
+    return writeSum;
+}
+
+int get_type(string &ask){
+    int n = ask.size();
+    if(n == 0) 
+        return 0;
+    int count_rn = 0;
+    int i;
+    for(i = 0; i < n; ++i){
+        if(count_rn == 2) 
+            break;
+        if(i + 1 < n && ask[i] == '\r' && ask[i + 1] == '\n'){
+            ++count_rn;
+            ++i;
+            continue;
+        }
+    }
+    if(count_rn < 2 || i >= n) 
+        return 0;
+    if(ask[i] == 'G')
+        return 1;
+    if(ask[i] == 'S')
+        return 2;
+    if(ask[i] == 'D')
+        return 3;
+    return 0;
+}
+
+Order getOrder(std::string orderStr)
+{
+    Order order;
+    if(orderStr[0] != '*'){
+        order.op = NIL;
+        return order;
+    }
+    std::vector<std::string> orderList;
+    const char *source = orderStr.c_str();
+    char delim[] = "\r\n";
+    char *input = strdup(source);
+    char *token = strsep(&input, delim);
+    while(token != NULL){
+        if(*token != '\0'){
+            orderList.emplace_back(std::string(token));
+        }
+        token = strsep(&input, delim);
+    }
+    free(input);
+    int num = orderList.size();
+    if(orderList[2] == "SET"){
+        order.op = SET;
+        order.key = orderList[4];
+        for (int i = 6; i < num; i += 2){
+            order.value.emplace_back(orderList[i]);
+        }
+    }
+    else if(orderList[2] == "GET"){
+        order.op = GET;
+        order.key = orderList[4];
+    }
+    else{
+        order.op = DEL;
+        for (int i = 4; i < num; i += 2){
+            order.value.emplace_back(orderList[i]);
+        }
+    }
+    return order;
+}
+
+std::string setOrder(Order order)
+{
+    std::string ret;
+    int num = order.value.size();
+    if (num == 0)
+    {
+        ret = "*1\r\n$3\r\nnil\r\n";
+        return ret;
+    }
+    std::string substr = "\r\n";
+    ret = "*";
+    ret = ret + std::to_string(num) + substr;
+    for (auto a : order.value)
+    {
+        num=a.size();
+        ret = ret + std::to_string(num) + substr + a + substr;
+    }
+    return ret;
+}
+
+
