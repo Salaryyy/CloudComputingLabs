384d5985a7ca8b501eafa94554e101c9a52152f1
Salaryyy <2639511626@qq.com>
2021-06-07 21:02:23 +0800

    Revert "feat: 加点功能"
    This reverts commit 5a4fb9f9591aa7ac83f9fccb4c927b17e8399f44.

--- Lab3/src/tools.h
+++ Lab3/src/tools.h
@@ -3,37 +3,22 @@
 #define ERROR_SIZE 256
 #include <string>
 #include <getopt.h>
-#include <vector>
-#define MAX_BUFF 1024
 
+//非法ip判断
+//bool get_IP_legal(regex_t *ipreg, const char *ip);
 
 // ip转换成int 不进行非法判断
 unsigned int ip_int(char *ip);
 
+// 通过文件名获取文件的类型
+const char *get_file_type(const char *name);
+
 // 读取http请求的一行
 int get_line(int sock, char *buf, int size);
 
-// 从输入参数获取配置文件信息 没有判断文件是否存在
+// 获取name和id
+bool get_name_id(char *str, int len, char *name, char *id);
+
 std::string getOptConf(int argc, char **argv);
-
-
-
-ssize_t readn(int fd, std::string &inBuffer);
-ssize_t writen(int fd, std::string &sbuff);
-
-// 获取请求类型
-int get_type(std::string &ask);
-
-enum Op{NIL, SET, GET, DEL};
-
-struct Order
-{
-    Op op;
-    std::string key;
-    std::vector<std::string> value;
-};
-
-Order getOrder(std::string orderStr);
-std::string setOrder(Order order);
 
 #endif
